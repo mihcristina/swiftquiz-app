0ce9a4ca473d885fb82a852a9ca3dd5ede2e5150
Michelli <contato@michellicristina.com.br>
2022-01-16 20:53:40 -0300

    initial configuration and add images in project

---
+++ swiftquiz-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftquiz-app
+//
+//  Created by Michelli Cristina de Paulo Lima on 16/01/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
